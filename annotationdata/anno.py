ad34dbc236ec0cd489f4eee08538b75cce402faf
qiwsir <qiwsir@gmail.com>
2015-01-31 18:28:18 +0800

    read data and store

--- annotationdata/anno.py
+++ annotationdata/anno.py
@@ -3,24 +3,29 @@
 
 import requests
 from bs4 import BeautifulSoup   # install BeautifulSoup: pip install beautifulsoup4
+import tarfile
+import wget
 
-import json
-#import csv
+import csv
 
-class ScratchData():
+class ScratchData(object):
 
-    def __init__(self, version, url):
+    def __init__(self, version, download_url, db_directory):
         self.version = version
-        self.url = url
+        self.url = download_url
+        self.db_dir = db_directory
 
     def scratchTable(self):
         """
         scratch table from web, and restore the name, title, link
         """
+        print "scratching...", self.url
+       
         r = requests.get(self.url)
         html_doc = r.text
         soup = BeautifulSoup(html_doc)
         table_content = soup.find("div", class_="do_not_rebase").find("table").find_all("tr")
+        
         log_lst = []
         for line in table_content:
             log_dict = {}
@@ -30,30 +35,24 @@ class ScratchData():
             for link in line.find_all('a'):
                 log_dict['package_link'] = unicode(link.get('href'))
             log_lst.append(log_dict)
+        
         return log_lst[1:]
     
     def usefulTable(self):
         content = self.scratchTable()
+        
         todo_lst = []
+        
         for line in content:
             package_name = line['package']
             if "." in package_name:
                 if package_name.split('.')[1] == 'db':
                     todo_lst.append(line)
+        
         return todo_lst
 
-    def writeLogs(self, logfile):
-        """
-        write logs
-        """
-        content = self.usefulTable()
-        with open(logfile, "wb") as lf:
-            lf.write(json.dumps(content))
 
-    def downloadFiles(self, restore_dir):
-        """
-        download the file by the link in the logs
-        """
+    def packageFilesUrl(self):
         source_link = self.usefulTable()
         db_link = [element['package_link'] for element in source_link ]
         link = ["http://bioconductor.org/packages/3.0/data/annotation/"+str(short_link) for short_link in db_link]
@@ -67,16 +66,43 @@ class ScratchData():
             all_link = "http://bioconductor.org/packages/3.0/data/annotation" + "".join(list(gz_link)[2:])
             file_link.append(all_link)
         
+        return file_link
+
+    def downloadPackage(self):
+        packages_link = self.packageFilesUrl()
+        
+        out_dir = self.db_dir
+        if out_dir[-1] != '/':
+            out_dir += "/"
+        
+        file_name_lst = []
+        for p_url in packages_link[1:3]:
+            print "downloading:\n"
+            file_name = wget.download(str(p_url), out=out_dir)
+            file_name_lst.append(file_name)
+        print "\nended download."
+        
+        return file_name_lst
 
 
     def unzipFile(self):
         """
         tar -xzvf the file
         """
-        pass
-
-    def readData(self, filesqlite):
-        """
-        read data from file of sqlite and restor to another csv
-        """
-        pass
+        gz_files = self.downloadPackage()
+        
+        db_dir = self.db_dir
+        
+        if db_dir[-1] != '/':
+            db_dir += '/'
+        
+        print "unzip the file(*.gz)"
+        
+        for filename in gz_files:
+            #path_filename = db_dir + filename
+            path_filename = filename
+            tar = tarfile.open(path_filename)
+            tar.extractall(path=db_dir)
+            tar.close()
+        
+        return gz_files 
--- annotationdata/application.py
+++ annotationdata/application.py
@@ -2,11 +2,21 @@
 # coding=utf-8
 
 from anno import *
+from sqlite import *
 
 def main():
     url = "http://bioconductor.org/packages/3.0/data/annotation/"
-    scratch = ScratchData(3, url)
+    db_dir = "/home/qw/Documents/Newgene/json-diff/annotationdata/db/"
+    log_file = "/home/qw/Documents/Newgene/json-diff/log.csv"
+    #scratch = ScratchData(3, url, db_dir)
+    #scratch.writeLogs(log_file)
     #scratch.writeLogs("log.txt")
-    print scratch.downloadFiles("ddd")
+    #scratch.downloadPackage("/home/qw/Documents/Newgene/json-diff/annotationdata/db/")
+    store_sql = StoreSqlite(version = 3, download_url=url,db_directory=db_dir,store_dir= db_dir)
+    #sqlites_dir = store_sql.storeSqlite()
+    #store_sql.writeLogs(log_file)
+    store_sql.listSqliteName()
+    #print sqlites_dir
+
 if __name__ == "__main__":
     main()
